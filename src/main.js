d4c2d29c7b75a9531039579048d9204ce7ec24ab
chansak04 <deawchansak@gmail.com>
2024-01-25 15:59:58 +0700

    masyer

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import route from '../src/router/index'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createApp(App).mount('#app')
+createApp(App).use(route).mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container my-5">
+        <div class="card shadow p-0" style="width: 18rem;">
+            <img src="https://miro.medium.com/v2/resize:fit:1400/1*EkXLPE4U3YDByeQmL7uy7g.png" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                    content.</p>
+                <a href="#" class="btn btn-primary">Read...</a>
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script setup>
+</script>
+
+<style lang="scss" scoped></style>
